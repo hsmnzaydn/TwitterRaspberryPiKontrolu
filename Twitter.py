cee2fc172164feee87c1b44e7ac7936fe20cae4c
Hüseyin Serkan Özaydin <serkan.zaydn@gmail.com>
2017-01-15 14:49:13 +0200

    Update Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -1,10 +1,10 @@
 import tweepy #tweepy kütüphanesini kullanacağımız için kütüphaneyi import ediyoruz
 import os #Led yakmak ve mp3 çaldırdığımız dosyaları çalıştırmak için kullanacağız
 import time #tweet apinin veri çekme aşımına düşmemek için 2 sn de bir tweetleri çekmek için kullanacağız
-consumer_key="SetTjt4D3KoBN3avFLmVBpZvE" #Twitter apinizin consumer keyiniz
-consumer_secret="HsvtteVP3Fu3x93AkqnnfaJwKBoDlzQQGniYLiNpe5vAfB6zZx" #Twitter apinizin consumer_secret keyiniz
-access_token="1629196130-Y9E1lA4sqwrRbpvHNIZL68QEvSsv7AiayKvCwCs"#Apinizin tokeni
-access_token_secret="Ngr4hoXQUISUdVqZV3OgXIHwr5sQ6YA2ZQ2G5jk5l0Sno"#Apinin token secreti
+consumer_key="" #Twitter apinizin consumer keyiniz
+consumer_secret="" #Twitter apinizin consumer_secret keyiniz
+access_token=""#Apinizin tokeni
+access_token_secret=""#Apinin token secreti
 
 giris = tweepy.OAuthHandler(consumer_key, consumer_secret)
 giris.set_access_token(access_token, access_token_secret)
